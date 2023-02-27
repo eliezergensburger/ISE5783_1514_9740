5273515fda76153f38dfec2e7a8418c9106b5c36
israeljacob <72940131+israeljacob@users.noreply.github.com>
2023-02-27 17:57:19 +0200

    Point

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,15 +1,19 @@
 package primitives;
 
-import java.util.Objects;
-
 public class Point {
-    final Double3 cordinate;
+    final Double3 coordinate;
 
-    public Point(Double3 cordinate) {
-        this.cordinate = cordinate;
+    public Point(Double3 coordinate) {
+        this.coordinate = coordinate;
     }
     Point(double x, double y, double z) {
-        this.cordinate = new Double3(x,y,z);
+        this.coordinate = new Double3(x,y,z);
+    }
+
+    @Override
+    public String toString() {
+        return "Point:\n" +
+                "\tcoordinate=" + coordinate;
     }
 
     @Override
@@ -17,6 +21,9 @@ public class Point {
         if (this == o) return true; // ?יכול להיות שזה מיותר
         if (o == null || getClass() != o.getClass()) return false;
         Point point = (Point) o;
-        return this.cordinate.equals(point);
+        return this.coordinate.equals(point);
+    }
+    public Point add(Vector v) {
+        return new Point(this.coordinate.add(v.coordinate));
     }
 }
---
+++ src/primitives/Vector.java
@@ -0,0 +1,11 @@
+package primitives;
+
+public class Vector extends Point {
+    public Vector(Double3 coordinate) {
+        super(coordinate);
+    }
+
+    Vector(double x, double y, double z) {
+        super(x, y, z);
+    }
+}
