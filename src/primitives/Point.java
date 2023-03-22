659e2ccd8d2b69b337286b262bd7a14816c0eaa5
israeljacob <72940131+israeljacob@users.noreply.github.com>
2023-03-22 18:38:24 +0200

    Started vector

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,8 +1,19 @@
 package primitives;
 
+import java.lang.Math;
+
+/**
+ * This class represents a point in space.
+ */
 public class Point {
+
+    /** The coordinate */
     final Double3 coordinate;
 
+    /**
+     * Constructs a Point object with the specified x, y, and z coordinates.
+     * @param coordinate a Double3 value representing the coordinates of the point.
+     */
     public Point(Double3 coordinate) {
         this.coordinate = coordinate;
     }
@@ -23,7 +34,41 @@ public class Point {
         Point point = (Point) o;
         return this.coordinate.equals(point);
     }
+
+    /**
+     * Returns a new Point object that is the result of adding the specified Vector object to this Point object.
+     * @param v the Vector object to add to this Point object.
+     * @return a new Point object that is the result of adding the specified Vector object to this Point object.
+     */
     public Point add(Vector v) {
         return new Point(this.coordinate.add(v.coordinate));
+    }
+
+    /**
+     * Returns a new Vector object that is the result of subtracting the specified Point object to this Point object.
+     * @param p the Point object to subtract from this Point object.
+     * @return a new Vector object that is the result of subtracting the specified Point object from this Point object.
+     */
+    public Vector subtract(Point p) {
+        return new Vector(this.coordinate.subtract(p.coordinate));
+    }
+
+    /**
+     * Returns the squared distance between two points.
+     * @param p the Point object to calculate the squared distance from.
+     * @return the squared distance between the points.
+     */
+    public double distanceSquared(Point p) {
+        Double3 temp = this.coordinate.subtract(p.coordinate);
+        return temp.d1 * temp.d1 + temp.d2 * temp.d2 + temp.d3 * temp.d3;
+    }
+
+    /**
+     * Returns the distance between two points.
+     * @param p the Point object to calculate the distance from.
+     * @return the distance between the points.
+     */
+    public double distance(Point p) {
+        return java.lang.Math.sqrt(this.distanceSquared(p));
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,9 +3,18 @@ package primitives;
 public class Vector extends Point {
     public Vector(Double3 coordinate) {
         super(coordinate);
+        if (coordinate.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Can't define zero vector");
     }
 
     Vector(double x, double y, double z) {
         super(x, y, z);
+        if (this.coordinate.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Can't define zero vector");
     }
+
+    public Vector scale(double d) {
+        return new Vector(this.coordinate.scale(d));
+    }
+
 }
