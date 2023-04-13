e31f09517f0c6babf691a2eba5e141f6d47dbb6a
Avraham Meiri <ameiri181@gmail.com>
2023-04-13 20:28:23 +0300

    Tests added

---
+++ unitTests/primitives/PointTest.java
@@ -0,0 +1,45 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testAdd() {
+        Point p1 = new Point(1, 2, 3);
+        assertEquals(
+                new Point(0, 0, 0),
+                p1.add(new Vector(-1, -2, -3)),
+                "ERROR: Point + Vector does not work correctly");
+
+        assertEquals(
+                new Point(2, 3, 4),
+                p1.add(new Vector(1, 1, 1)),
+                "ERROR: Point + Vector does not work correctly");
+
+
+    }
+
+    @Test
+    void testSubtract() {
+    }
+
+    @Test
+    void testDistanceSquared() {
+    }
+
+    @Test
+    void testDistance() {
+    }
+}
