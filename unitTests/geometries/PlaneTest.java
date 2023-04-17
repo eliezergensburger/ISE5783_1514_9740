d48c4488126ab95b84b3ac1d8cc5bc4ecc8d6616
israeljacob <72940131+israeljacob@users.noreply.github.com>
2023-04-17 16:28:38 +0300

    Plane constructor Test

--- unitTests/geometries/PlaneTest.java
+++ unitTests/geometries/PlaneTest.java
@@ -8,6 +8,42 @@ import static org.junit.jupiter.api.Assertions.*;
 import static primitives.Util.isZero;
 
 class PlaneTest {
+
+    @Test
+    void testConstractor(){
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: Test for a proper result.
+        try{
+            new Plane(new Point(1,0,0),new Point(0,1,0),new Point(0,0,1));
+        }catch (IllegalArgumentException e){
+            // This is a fail test. It is a test that is expected to fail.
+            fail("The constarctor throw error for nothing");
+        }
+        // ============ Boundary Values Tests =============
+        // TC02: Test when all points arr equal.
+        assertThrows(IllegalArgumentException.class,
+                ()->new Plane(new Point(1,0,0),new Point(1,0,0),new Point(1,0,0)),
+                "The constractor dont throw error exception for the same three points");
+        // TC03: Test when b point equals to a point.
+        assertThrows(IllegalArgumentException.class,
+                ()->new Plane(new Point(1,0,0),new Point(1,0,0),new Point(2,0,0)),
+                "The constractor dont throw error exception for the same three points");
+        // TC04: Test when a point equals to c point.
+        assertThrows(IllegalArgumentException.class,
+                ()->new Plane(new Point(1,0,0),new Point(2,0,0),new Point(1,0,0)),
+                "The constractor dont throw error exception for the same three points");
+        // TC05: Test when b point equals to c point.
+        assertThrows(IllegalArgumentException.class,
+                ()->new Plane(new Point(2,0,0),new Point(1,0,0),new Point(1,0,0)),
+                "The constractor dont throw error exception for the same three points");
+        // TC06: Test when all points arr on the same line.
+        assertThrows(IllegalArgumentException.class,
+                ()->new Plane(new Point(1,0,0),new Point(2,0,0),new Point(3,0,0)),
+                "The constractor dont throw error exception for the  three points in the same line");
+
+    }
+
+
     /**
      * Test method for {@link geometries.Polygon#getNormal(primitives.Point)}.
      * 1.checks if it is on the plane
